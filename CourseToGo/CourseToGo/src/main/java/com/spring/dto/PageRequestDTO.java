7a3d0f8936f39f4edb89a205ded8306db20fa7b4
wellbeing98 <tom9816@gmail.com>
2023-07-17 17:25:25 +0900

    UPDATE CourseCOntroller, PageRequestDTO, CourseMapper.xml 찜하기 기능관련 DTO 컨트롤러 수정 xml 포함

--- CourseToGo/CourseToGo/src/main/java/com/spring/dto/PageRequestDTO.java
+++ CourseToGo/CourseToGo/src/main/java/com/spring/dto/PageRequestDTO.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class PageRequestDTO{
 	//현재 페이지 번호
 	private int pageNum;
-	
+	private int userId;
 	private String keyword;
 	//페이지당 출력할 데이터 개수
 	private int amount;
